171f648070505d70bfc6391c9317cc4561f222e5
ReyTroan <reytroan@gmail.com>
2023-11-23 12:06:03 +0300

    - adding login input

--- app/src/main/java/com/example/notesapp/LoginFragment.kt
+++ app/src/main/java/com/example/notesapp/LoginFragment.kt
@@ -5,29 +5,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.notesapp.databinding.FragmentOnboardingBinding
+import com.example.notesapp.databinding.FragmentLoginBinding
 
-class OnboardingFragment : Fragment() {
+class LoginFragment: Fragment() {
 
-    private var binding : FragmentOnboardingBinding? = null
+    private var binding : FragmentLoginBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentOnboardingBinding.inflate(inflater, container, false)
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
         return binding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding?.viewPager?.adapter = OnboardingAdapter(parentFragmentManager)
-        binding?.circleIndicator?.setViewPager(binding?.viewPager)
-        binding?.skipButton?.setOnClickListener {
+        binding?.enterLogin?.setOnClickListener{
             parentFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, LoginFragment())
+                .replace(R.id.fragment_container, MainFragment())
                 .commit()
         }
     }
+
 }
